eaf07dc2f021f281c9be5261a79693a22a87dc78
Oliver0731 <o.townell@share.epsb.ca>
2024-03-08 09:30:14 -0700

    update

--- Grade Calculator GUI/main.js
+++ Grade Calculator GUI/main.js
@@ -9,7 +9,19 @@ function calculate() {
   let structuredprogramming2 = document.getElementById("StructP2").value;
   let ClientScript2 = document.getElementById("Client-side2").value;
   let progectA = document.getElementById("ProA").value;
-  //PROCESS
+  //idivdual grade:
+  document.getElementById("clientOut1").innerHTML =
+    getLetterGrade(ClientScript1);
+  document.getElementById("struckOut1").innerHTML = getLetterGrade(
+    structeredProgramming1
+  );
+  document.getElementById("structOut2").innerHTML = getLetterGrade(
+    structuredprogramming2
+  );
+  document.getElementById("ClientOut2").innerHTML =
+    getLetterGrade(ClientScript2);
+  document.getElementById("proOutA").innerHTML = getLetterGrade(progectA);
+  //total grade
   let CSTotal =
     (+ClientScript1 +
       +structeredProgramming1 +
@@ -17,24 +29,21 @@ function calculate() {
       +ClientScript2 +
       +progectA) /
     5;
-    //change html
-    let Letter = getLetterGrade(CSTotal);
-    document.getElementById("foot").innerHTML = `Letter grade: ${Letter}`;
+  //change html
+  let Letter = getLetterGrade(CSTotal);
+  document.getElementById("foot").innerHTML = `Final CS10 Grade: ${CSTotal}%`;
 }
 // claculate letter
-function getLetterGrade(percentGrade){
-if  (percentGrade >= 90) {
-  return "A";
-} else if (percentGrade >= 75 && percentGrade < 90){
-return "B";
-} else if (percentGrade >= 60 && percentGrade < 75){
-  return "C";
-} else if (percentGrade >= 50 && percentGrade < 60){
-  return "D";
-} else if ( percentGrade >= 0 && percentGrade < 50){
-  return "F";
+function getLetterGrade(percentGrade) {
+  if (percentGrade >= 90) {
+    return "A";
+  } else if (percentGrade >= 75 && percentGrade < 90) {
+    return "B";
+  } else if (percentGrade >= 60 && percentGrade < 75) {
+    return "C";
+  } else if (percentGrade >= 50 && percentGrade < 60) {
+    return "D";
+  } else if (percentGrade >= 0 && percentGrade < 50) {
+    return "F";
+  }
 }
-}
-
-
-
